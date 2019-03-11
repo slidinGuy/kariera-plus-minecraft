9c603b62c2c066bfe28bcde4adc0ac5098ddf94a
slidinGuy <31001275+slidinGuy@users.noreply.github.com>
2019-03-11 19:07:54 +0100

    Update kariera.js

--- kariera.js
+++ kariera.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Kariera Plus
 // @namespace    http://tampermonkey.net/
-// @version      0.3
+// @version      0.4
 // @description  try to take over the world!
 // @author       You
 // @match        *://*/*
